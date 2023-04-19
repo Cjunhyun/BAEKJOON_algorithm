2e367a05cb258b41b5f28b4603862c84dfd40e23
Cjunhyun <48709581+Cjunhyun@users.noreply.github.com>
2023-04-19 19:20:24 +0900

    Update #백준 단계별로 풀어보기 5.py

--- #백준 단계별로 풀어보기 5.py
+++ #백준 단계별로 풀어보기 5.py
@@ -82,3 +82,47 @@ elif(k[0] == ' ' or k[len(k) - 1 ] == ' '):
     print(cnt - 1)
 else:
     print(cnt)
+
+# 2908번
+nums = list(input().split())
+a = list(nums[0])
+b = list(nums[1])
+
+a.reverse()
+b.reverse()
+
+res_a = ''.join(str(s) for s in a)
+res_b = ''.join(str(s) for s in b)
+int(res_a)
+int(res_b)
+if(res_a > res_b):
+    print(res_a)
+else:
+    print(res_b)
+
+# 5622번
+num_time =  e = {'a':3,'b':3,'c':3,'d':4,'e':4,'f':4,'g':5,'h':5,'i':5,'j':6
+      ,'k':6,'l':6,'m':7,'n':7,'o':7,'p':8,'q':8,'r':8,'s':8,'t':9,'u':9,'v':9
+      ,'w':10,'x':10,'y':10,'z':10}
+
+s = input()
+s = s.lower()
+s = list(s)
+cnt  = 0
+for i in s:
+    cnt += e[i]
+print(cnt)
+
+# 11718번
+a = 1
+while(True):
+    try:
+        while(a==1):
+            s = input()
+            if(s == ''):
+                a = 0
+            else:
+                print(s)
+    except EOFError:
+     break
+
